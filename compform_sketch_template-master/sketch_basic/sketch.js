b34f6e214ce41bd145b4acf89f485cc7a3908a17
Jennifer Lee <jenn.lee1794@gmail.com>
2017-02-04 17:52:01 -0500

    add correct sketch_basic folder

--- compform_sketch_template-master/sketch_basic/sketch.js
+++ compform_sketch_template-master/sketch_basic/sketch.js
@@ -2,21 +2,19 @@
 
 function setup() {
 	// create a place to draw
-	createCanvas(600, 600);
+	createCanvas(640, 360);
 }
 
 
 function draw() {
 	// clear the background
-	background(100, 255, 130);
+	background(0, 0, 0);
 
 	// set drawing styles
-
+	stroke(255, 0, 0);
+	fill(255, 255, 255);
+	strokeWeight(10);
 
 	// draw a circle
-	fill(random(255),random(255),random(255))
-	ellipse(random(400), 80, 100, 100);
-
-	noLoop();
-
+	ellipse(320, 180, 100, 100);
 }
